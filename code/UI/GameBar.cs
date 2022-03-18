716ebfa1a3f68d4ec44cc2acdfe5b24ca62996ac
That_Lone_Wanderer <matthew.rigz@outlook.com>
2022-03-18 22:28:56 +0000

    More comments

--- code/UI/GameBar.cs
+++ code/UI/GameBar.cs
@@ -11,7 +11,10 @@ namespace Sandbox.UI
 	public class GameBar : Panel
 	{
 		//The names of these properties MUST MATCH the names of the @ref classes in the HTML file
+		//The GET AND SET IS 100% NECESSARY OTHERWISE IT WILL NOT BIND TO THE HTML CORRECTLY.
 		public Label Header { get; set; }
+
+		//This property has the same names as the @ref in the html. BUT THE CLASS NAME CAN BE DIFFERENT IN THE HTML AS THAT CAN BE CUSTOMISED IN SCSS
 		public Button btn { get; set; }
 		public GameBar()
 		{
